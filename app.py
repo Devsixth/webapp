7c2c5b76c35c984dad75fca86ffa2fb6af96b057
Devsixth <129255859+Devsixth@users.noreply.github.com>
2023-09-01 10:09:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 project_id = "64e6e1bc184f94861801"
 
 app = Flask(__name__, static_url_path='/static')
-app.secret_key = os.urandom(24)
+app.secret_key = '123456789'
 login_manager = LoginManager()
 login_manager.init_app(app)
 
